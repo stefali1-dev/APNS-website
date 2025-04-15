eccc2945532d5756913939cf6419fd0acad713ba
stefali1-dev <stefanleustean56@gmail.com>
2025-04-15 22:59:01 +0300

    Work on navbar and email subscription

--- django_sample/common/urls.py
+++ django_sample/common/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
+    path('subscribe/', views.subscribe, name='subscribe'),
     path('enrollment', views.enrollment_view, name='enrollment_page'),
     path('donation', views.donation_view, name='donation_page'),
     path('contact', views.contact_view, name='contact_page'),
--- django_sample/common/views.py
+++ django_sample/common/views.py
@@ -2,11 +2,27 @@ from django.shortcuts import render
 
 from blog.models import Post
 
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from users.models import EmailSubscription
+
 # Create your views here.
 # views.py
 def landing_page(request):
     featured_posts = Post.objects.filter(show_on_landing=True).order_by('-created_at')[:3]  # Get 6 posts max
     return render(request, 'common/landing_page.html', {'featured_posts': featured_posts})
+
+@require_POST
+def subscribe(request):
+    email = request.POST.get('email', '')
+    try:
+        EmailSubscription.objects.create(email=email)
+        response = JsonResponse({'status': 'success'})
+        # Set cookie for 30 days
+        response.set_cookie('email_subscribed', 'true', max_age=30*24*60*60)
+        return response
+    except Exception as e:
+        return JsonResponse({'status': 'error', 'message': str(e)}, status=400)
 
 def enrollment_view(request):
     context = {
